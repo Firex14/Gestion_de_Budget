c8d954ad27d2ae062f2b19eb4a3421ed7d8ce784
yaba <gueyeyaba207@gmail.com>
2023-02-05 23:13:43 +0000

    first commit

---
+++ src/main/java/com/yaba/monbudget/MonBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.yaba.monbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaba/monbudget/MonBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaba.monbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
